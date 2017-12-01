44bd3fd8742d618354d441c63f4c17e1e43986f2
Jeremy Downs <Jeremy.L.Downs@gmail.com>
2017-12-01 18:09:17 -0500

    Redefined Person class

--- schema_org.py
+++ schema_org.py
@@ -33,60 +33,21 @@ class Action(Thing):
         for attribute in self.canonicalAttributes:
             setattr(self, attribute, kwargs.get(attribute, None))
 
-
 class Person(Thing):
     def __init__(self, **kwargs):
         super().__init__()
-        print("__init__ Person")
-        self.additionalName = kwargs.get('additionalName', default=None)
-        self.address = kwargs.get('address', default=None)
-        self.affiliation = kwargs.get('affiliation', default=None)
-        self.alumniOf = kwargs.get('alumniOf', default=None)
-        self.award = kwargs.get('award', default=None)
-        self.birthDate = kwargs.get('birthDate', default=None)
-        self.birthPlace = kwargs.get('birthPlace', default=None)
-        self.brand = kwargs.get('brand', default=None)
-        self.children = kwargs.get('children', default=None)
-        self.colleague = kwargs.get('colleague', default=None)
-        self.contactPoint = kwargs.get('contactPoint', default=None)
-        self.deathDate = kwargs.get('deathDate', default=None)
-        self.deathPlace = kwargs.get('deathPlace', default=None)
-        self.duns = kwargs.get('duns', default=None)
-        self.email = kwargs.get('email', default=None)
-        self.familyName = kwargs.get('familyName', default=None)
-        self.faxNumber = kwargs.get('faxNumber', default=None)
-        self.follows = kwargs.get('follows', default=None)
-        self.funder = kwargs.get('funder', default=None)
-        self.gender = kwargs.get('gender', default=None)
-        self.givenName = kwargs.get('givenName', default=None)
-        self.globalLocation = kwargs.get('globalLocation', default=None)
-        self.hasOccupation = kwargs.get('hasOccupation', default=None)
-        self.hasOfferCatalog = kwargs.get('hasOfferCatalog', default=None)
-        self.hasPOS = kwargs.get('hasPOS', default=None)
-        self.height = kwargs.get('height', default=None)
-        self.homeLocation = kwargs.get('homeLocation', default=None)
-        self.honorificPrefix = kwargs.get('honorificPrefix', default=None)
-        self.honorificSuffic = kwargs.get('honorificSuffic', default=None)
-        self.isicV4 = kwargs.get('isicV4', default=None)
-        self.jobTitle = kwargs.get('jobTitle', default=None)
-        self.knows = kwargs.get('knows', default=None)
-        self.makesOffer = kwargs.get('makesOffer', default=None)
-        self.memberOf = kwargs.get('additionalName', default=None)
-        self.naics = kwargs.get('additionalName', default=None)
-        self.nationality = kwargs.get('additionalName', default=None)
-        self.netWorth = kwargs.get('additionalName', default=None)
-        self.owns = kwargs.get('additionalName', default=None)
-        self.parent = kwargs.get('additionalName', default=None)
-        self.performerIn = kwargs.get('additionalName', default=None)
-        self.publishingPrinciples = kwargs.get('additionalName', default=None)
-        self.relatedTo = kwargs.get('additionalName', default=None)
-        self.seeks = kwargs.get('additionalName', default=None)
-        self.sibling = kwargs.get('additionalName', default=None)
-        self.sponsor = kwargs.get('additionalName', default=None)
-        self.spouse = kwargs.get('additionalName', default=None)
-        self.taxID = kwargs.get('additionalName', default=None)
-        self.telephone = kwargs.get('additionalName', default=None)
-        self.vatID = kwargs.get('additionalName', default=None)
-        self.weight = kwargs.get('additionalName', default=None)
-        self.workLocation = kwargs.get('additionalName', default=None)
-        self.worksFor = kwargs.get('additionalName', default=None)
+        self.canonicalAttributes = ('additionalName', 'address', 'affiliation',
+            'alumniOf', 'award', 'birthDate', 'birthPlace', 'brand', 'children',
+            'colleague', 'contactPoint', 'deathDate', 'duns', 'email',
+            'familyName', 'faxNumber', 'follows', 'funder', 'gender', 'givenName',
+            'globalLocation', 'hasOccupation', 'hasOfferCatalog', 'hasPOS',
+            'height', 'homeLocation', 'honorificPrefix', 'honorificSuffix',
+            'isicV4', 'jobTitle', 'knows', 'makesOffer', 'memberOf', 'naics',
+            'nationality', 'netWorth', 'owns', 'parent', 'performerIn',
+            'publishingPrinciples', 'relatedTo', 'seeks', 'sibling', 'sponsor',
+            'spouse', 'taxID', 'telephone', 'vatID', 'weight', 'workLocation',
+            'worksFor')
+        self.itemType = "Person"
+        self.definitionLastUpdated = datetime.date(2017, 12, 1)
+        for attribute in self.canonicalAttributes:
+            setattr(self, attribute, kwargs.get(attribute, None))
